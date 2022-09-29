b4d20640871dc89526ecf545d0422046cf5dca31
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-29 17:38:05 +0200

    updated dependencies

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from starlite import Starlite, get
 
 @get("/")
 def hello_world() -> dict[str, Any]:
-    """Hi."""
+    """Route Handler that outputs hello world."""
     return {"hello": "world"}
 
 
